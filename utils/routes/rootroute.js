7c4bef7a5cd3e902bd13c09e4c1cc7d41a09c961
prasannashrestha011 <shresthaprashanna27@gmail.com>
2024-02-11 20:51:47 +0545

    info route was added

--- utils/routes/rootroute.js
+++ utils/routes/rootroute.js
@@ -4,8 +4,10 @@ const route=Router()
 const getRoute=require('./get.js')
 const putpatch=require('./patch&put.js')
 const postRoute=require('./post.js')
+const infoRoute=require('./info.js')
 route.use(getRoute)
 route.use(putpatch)
 route.use(postRoute)
+route.use(infoRoute)
 
 module.exports=route
