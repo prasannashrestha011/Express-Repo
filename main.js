76110a980d0630db0b9c85b59c2138f2aebeff99
prasannashrestha011 <shresthaprashanna27@gmail.com>
2024-02-12 20:56:48 +0545

    main.js

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ const app=express()
 app.use(express.json())
 const PORT=3000
 const parsecookie=require('cookie-parser')
+const session=require('express-session')
+app.use(session({
+    secret:"Hello123",
+    saveUninitialized:false,
+    resave:false
+})) /// global decleration of session ID, it will assign the session id to the new http request automatically
 
 const {query,validationResult,body,matchedData,checkSchema}=require('express-validator')
 const schema=require('./utils/schema.js')
@@ -14,9 +20,17 @@ const logMiddleware=require('./utils/middleware.js')//middleware function
 const userLOG=require('./utils/static.js') // users details
 app.use(parsecookie('HelloWorld')); // Use cookie-parser middleware
 app.get('/world',(req,res)=>{
+    console.log(req.sessionID)
+    console.log(req.session)
+    req.session.authentication=true
     res.cookie('Hello','World',{maxAge:60000*60*2,signed:true})
     console.log(req.cookies)
     res.send('Cookie set')
+})
+app.get('/create',(req,res)=>{
+    const sessionID=req.session.id
+    console.log(sessionID)
+    res.send(`Your session ID has been created ${sessionID}` )
 })
  app.use(Routes)
 
@@ -27,6 +41,35 @@ app.delete('/users/:id',logMiddleware,(req,res)=>{
 })
 app.get('/all',(req,res)=>{
     res.send(userLOG)
+})
+app.post('/auth',(req,res)=>{
+    const {body:{user,address}}=req;
+    const findUser=userLOG.find((User)=>{
+         return User.user==user;
+    })
+    if(findUser.address!=address) return res.status(401).send("Invalid info")
+    req.session.user=findUser
+    console.log(req.sessionID)
+    console.log(req.session)
+    res.send(findUser)
+})
+app.post('/cart',(req,res)=>{
+    if(!req.session.user) return res.status(401).send('Please authenticate your ID')
+    const {body}=req;
+    const {cart}=req.session;
+    if(cart)
+    {
+        cart.push(body)
+    }
+    else{
+        req.session.cart=[body]
+    }
+    res.send("Your product has been added to chart")
+})
+app.get('/cart',(req,res)=>{
+    if(!req.session.user) return res.status(401).send('Please authenticate yourself') // authentication
+    const { cart }=req.session
+    res.send(cart)
 })
 app.get('/users/:id',logMiddleware,(req,res)=>{
     const userID=req.FindUser;
